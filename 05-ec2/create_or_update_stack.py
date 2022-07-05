fc57c096bf34cf18dbf20372d0d4df9825f31af4
Allen Fisher <allen.fisher@stelligent.com>
2022-07-05 09:30:53 -0700

    Lab 5.1.2 functional

--- 05-ec2/create_or_update_stack.py
+++ 05-ec2/create_or_update_stack.py
@@ -1,10 +1,9 @@
 import boto3
+from botocore.config import Config
+
 import argparse
-import yaml
 import json
 
-
-cf = boto3.client('cloudformation')
 
 def _stack_exists(stack_name):
     stacks = cf.list_stacks().get('StackSummaries')
@@ -19,35 +18,52 @@ def _stack_exists(stack_name):
 def _process_template(template_file):
     with open(template_file) as template:
         template_data = template.read()
-    return cf.validate_template(TemplateBody=template_data)
+    return template_data
 
 
 def _process_parameters(parameter_file):
-    with open(parameter_file) as parameters:
-        parameter_data = json.load(parameters)
-    return parameter_data
+    with open(args.parameter_file) as f:
+        data = json.load(f)
+    return data
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description='Create or update a stack using AWS CloudFormation')
 
-    arg_parser.add_argument('-n', '--stack_name',       description='Name of the stack to create or update',    required=True)
-    arg_parser.add_argument('-t', '--template_file',    description='Path to file in YAML format',              required=True)
-    arg_parser.add_argument('-p', '--parameter_file',   description='Parameter file for template parameters.',  required=True)
+    arg_parser.add_argument('-n', '--stack_name',       help='Name of the stack to create or update',    required=True)
+    arg_parser.add_argument('-t', '--template_file',    help='Path to file in YAML format',              required=True)
+    arg_parser.add_argument('-p', '--parameter_file',   help='Parameter file for template parameters.',  required=True)
+    arg_parser.add_argument('-r', '--region',           help='Region to deploy the stack in',            required=True)
     args = arg_parser.parse_args()
+
+    config = Config(
+        region_name = args.region
+    )
+    
+    cf = boto3.client('cloudformation', config=config)
 
     params = {
         'StackName': args.stack_name,
         'TemplateBody': _process_template(args.template_file),
         'Parameters': _process_parameters(args.parameter_file),
-    }    
+    } 
+
     if _stack_exists(args.stack_name):
             print(f'Updating {args.stack_name}')
-            stack_result = cf.update_stack(**params)
+            resp = cf.update_stack(
+                StackName=args.stack_name,
+                TemplateBody=_process_template(args.template_file),
+                Parameters=_process_parameters(args.parameter_file)
+            )
             waiter = cf.get_waiter('stack_update_complete')
     else:
         print(f'Creating {args.stack_name}')
-        stack_result = cf.create_stack(**params)
+        resp = cf.create_stack(
+            StackName=args.stack_name,
+            TemplateBody=_process_template(args.template_file),
+            Parameters=_process_parameters(args.parameter_file)
+        )
         waiter = cf.get_waiter('stack_create_complete')
         
-        print("...waiting for stack to be ready...")
-        waiter.wait(StackName=args.stack_name)
+        print(f'Waiting for stack {args.stack_name} to be ready')
+    
+    waiter.wait(StackName=args.stack_name)
